470f954967dab5e3f7a247ef49d6d98b610ba6f7
Stephen <sjgowen@gmail.com>
2025-05-23 11:48:22 +0100

    Challenge2

---
+++ Assets/Challenge 2/Scripts/PrefabExistance.cs
@@ -0,0 +1,6 @@
+public static class PrefabExistance
+{
+    private static bool _dogExists;
+
+    public static bool dogExists { get => _dogExists; set => _dogExists = value; }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnIntervalMin = 3.0f;
+    private float spawnIntervalMax = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        ScheduleNextInvoke(startDelay);
+    }
+
+    private void ScheduleNextInvoke(float startDelay = 0)
+    {
+        float randomTime = Random.Range(spawnIntervalMin, spawnIntervalMax);
+        Invoke(nameof(SpawnRandomBall), randomTime + startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        GameObject ball = ballPrefabs[Random.Range(0, ballPrefabs.Length)];
+        Instantiate(ball, spawnPos, ball.transform.rotation);
+        ScheduleNextInvoke();
+    }
+
+}
